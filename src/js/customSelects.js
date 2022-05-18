02a21a0b02ba28df4e2a9cd4b730a452bcb9bb94
Artem Safin <artemsafin.webdev@gmail.com>
2022-05-19 00:48:17 +0300

    Новые блоки плюс вывел инициализацию селектов

--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -1,14 +1,21 @@
 import Choices from 'choices.js';
 
 export default function customSelects() {
-    const customSelects = Array.from(document.querySelectorAll('.js-custom-select'));
+    function initializeSelects() {
+        const customSelects = Array.from(document.querySelectorAll('.js-custom-select'));
 
-    customSelects.forEach((select) => {
-        new Choices(select, {
-            searchEnabled: false,
-            itemSelectText: '',
-            shouldSort: false,
-            position: select.hasAttribute('data-position') ? select.getAttribute('data-position') : 'top'
+        customSelects.forEach((select) => {
+            new Choices(select, {
+                searchEnabled: false,
+                itemSelectText: '',
+                shouldSort: false,
+                position: select.hasAttribute('data-position') ? select.getAttribute('data-position') : 'top'
+            });
         });
-    });
+    }
+
+    initializeSelects();
+
+    window.initializeSelects = initializeSelects;
+    
 }
